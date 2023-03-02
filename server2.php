01461bfb8d14a7bb0b0f1ccc122930b8eafe5556
ismail <ismailkharrobi1@gmail.com>
2023-03-02 17:54:41 +0100

    improve socket stability

--- server2.php
+++ server2.php
@@ -2,7 +2,7 @@
 
 if (isset($_GET['id'])):
     $id = $_GET['id'];
-    $url = 'http://'.getenv('CND_IP').':'.getenv('CDN_PORT').'/'.$id; 
+    $url = 'http://'.getenv('CDN_IP').':'.getenv('CDN_PORT').'/'.$id; 
     $save_as = $id; 
 
     $contents = file_get_contents($url);
